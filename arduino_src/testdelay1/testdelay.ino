48184e76287163b490849b7639c56775c9da82e5
QimingTJU <3020001749@tju.edu.cn>
2021-11-27 14:01:12 +0800

    小车转向

--- arduino_src/testdelay1/testdelay.ino
+++ arduino_src/testdelay1/testdelay.ino
@@ -1,74 +1,128 @@
+// digitalWrite(R_IN1, LOW);       //这个是向前跑的代码,放在前面示例
+// digitalWrite(R_IN2, HIGH);
+// digitalWrite(L_IN3, LOW);
+// digitalWrite(L_IN4, HIGH);
 
+//初始化引脚
+int R_IN1 = 6;
+int R_IN2 = 7;
+int L_IN3 = 4;
+int L_IN4 = 5;
 
-//初始化
-int In1 = 6;
-int In2 = 7;
-int In3 = 4;
-int In4 = 5;
-
-int Sensor_x1 = 11;
-
-int Sensor_x2 = 10;
-int Sensor_x3 = 9;
-int Sensor_x4 = 8;
-
+int Sensor_L_X1 = 11;
+int Sensor_LL_X2 = 10;
+int Sensor_R_X3 = 9;
+int Sensor_RR_X4 = 8;
 
 
 void setup() 
 {
   // 初始化I/O口
-    pinMode(In1,OUTPUT);
-    pinMode(In2,OUTPUT);
-    pinMode(In3,OUTPUT);
-    pinMode(In4,OUTPUT);
+    pinMode(R_IN1,OUTPUT);
+    pinMode(R_IN2,OUTPUT);
+    pinMode(L_IN3,OUTPUT);
+    pinMode(L_IN4,OUTPUT);
 
-    pinMode(Sensor_x1,INPUT);
-    pinMode(Sensor_x2,INPUT);
-    pinMode(Sensor_x3,INPUT);
-    pinMode(Sensor_x4,INPUT);
+    pinMode(Sensor_L_X1,INPUT);
+    pinMode(Sensor_LL_X2,INPUT);
+    pinMode(Sensor_R_X3,INPUT);
+    pinMode(Sensor_RR_X4,INPUT);
 
     Serial.begin(115200);
 
 }
 
+//几个转向函数
+void Go_Straight()
+{
+    digitalWrite(R_IN1, LOW);
+    digitalWrite(R_IN2, HIGH);
+    digitalWrite(L_IN3, LOW);
+    digitalWrite(L_IN4, HIGH);
+}
+
+void Slow_TurnLeft()
+{
+    digitalWrite(R_IN1,LOW);
+    analogWrite(R_IN2,255);
+    digitalWrite(L_IN3,LOW);
+    analogWrite(L_IN4,220);
+}
+
+void Slow_TurnRight()
+{
+    digitalWrite(R_IN1,LOW);
+    analogWrite(R_IN2,220);
+    digitalWrite(L_IN3,LOW);
+    analogWrite(L_IN4,255);
+}
+
+void Fast_TurnLeft()
+{
+    digitalWrite(R_IN1,LOW);
+    analogWrite(R_IN2,255);
+    digitalWrite(L_IN3,LOW);
+    analogWrite(L_IN4,200);
+}
+
+void Fast_TurnRight()
+{
+    digitalWrite(R_IN1,LOW);
+    analogWrite(R_IN2,200);
+    digitalWrite(L_IN3,LOW);
+    analogWrite(L_IN4,255);
+}
+
+void Stop()
+{
+    digitalWrite(R_IN1, LOW);
+    digitalWrite(R_IN2, LOW);
+    digitalWrite(L_IN3, LOW);
+    digitalWrite(L_IN4, LOW); 
+}
+
+
+
+//现在有两种算法，一种是单纯if判断，另一种是给每个传感器赋个特殊的值，然后用switch语句
+//先把if的写了
+
 void loop() 
 {
-    int SensorVal_x1 = digitalRead(Sensor_x1);
-    int SensorVal_x2 = digitalRead(Sensor_x2);
-    int SensorVal_x3 = digitalRead(Sensor_x3);
-    int SensorVal_x4 = digitalRead(Sensor_x4);
+    int SensorVal_L_X1 = digitalRead(Sensor_L_X1);
+    int SensorVal_LL_X2 = digitalRead(Sensor_LL_X2);
+    int SensorVal_R_X3 = digitalRead(Sensor_R_X3);
+    int SensorVal_RR_X4 = digitalRead(Sensor_RR_X4);
 
-    //控制正反转
-    if (SensorVal_x1  && SensorVal_x3)
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 0 && SensorVal_R_X3 == 1 && SensorVal_RR_X4 == 1)
     {
-        digitalWrite(In1, LOW);
-        digitalWrite(In2, HIGH);
-        digitalWrite(In3, LOW);
-        digitalWrite(In4, HIGH);
-    }
-    else
-    {
-        digitalWrite(In1, LOW);
-        digitalWrite(In2, LOW);
-        digitalWrite(In3, LOW);
-        digitalWrite(In4, LOW);
-
+        Slow_TurnLeft();
     }
 
-    delay(10);
-    // else
-    // {
-    //     digitalWrite(In1,HIGH);
-    //     digitalWrite(In2,LOW);
-    // }
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 1 && SensorVal_R_X3 == 0 && SensorVal_RR_X4 == 1)
+    {
+        Slow_TurnRight();
+    }
 
+    if(SensorVal_LL_X2 == 0 && SensorVal_L_X1 == 1 && SensorVal_R_X3 == 1 && SensorVal_RR_X4 == 1)
+    {
+        Fast_TurnLeft();
+    }
 
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 1 && SensorVal_R_X3 == 1 && SensorVal_RR_X4 == 0)
+    {
+        Fast_TurnRight();
+    }
 
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 0 && SensorVal_R_X3 == 0 && SensorVal_RR_X4 == 1)
+    {
+        Go_Straight();
+    }
 
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 1 && SensorVal_R_X3 == 1 && SensorVal_RR_X4 == 1)
+    {
+        Stop();
+    }
 
+    delay(1);
 
-
-
-    // digitalWrite(4,LOW);
-    // digitalWrite(5,HIGH);
 }
--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,67 +1,129 @@
 # 1 "d:\\arduino\\arduino_src\\testdelay1\\testdelay.ino"
+// digitalWrite(R_IN1, LOW);       //这个是向前跑的代码,放在前面示例
+// digitalWrite(R_IN2, HIGH);
+// digitalWrite(L_IN3, LOW);
+// digitalWrite(L_IN4, HIGH);
 
+//初始化引脚
+int R_IN1 = 6;
+int R_IN2 = 7;
+int L_IN3 = 4;
+int L_IN4 = 5;
 
-//初始化
-int In1 = 6;
-int In2 = 7;
-int In3 = 4;
-int In4 = 5;
-
-int Sensor_x1 = 11;
-int Sensor_x2 = 10;
-int Sensor_x3 = 9;
-int Sensor_x4 = 8;
-
+int Sensor_L_X1 = 11;
+int Sensor_LL_X2 = 10;
+int Sensor_R_X3 = 9;
+int Sensor_RR_X4 = 8;
 
 
 void setup()
 {
   // 初始化I/O口
-    pinMode(In1,0x1);
-    pinMode(In2,0x1);
-    pinMode(In3,0x1);
-    pinMode(In4,0x1);
+    pinMode(R_IN1,0x1);
+    pinMode(R_IN2,0x1);
+    pinMode(L_IN3,0x1);
+    pinMode(L_IN4,0x1);
 
-    pinMode(Sensor_x1,0x0);
-    pinMode(Sensor_x2,0x0);
-    pinMode(Sensor_x3,0x0);
-    pinMode(Sensor_x4,0x0);
+    pinMode(Sensor_L_X1,0x0);
+    pinMode(Sensor_LL_X2,0x0);
+    pinMode(Sensor_R_X3,0x0);
+    pinMode(Sensor_RR_X4,0x0);
 
     Serial.begin(115200);
 
 }
 
+//几个转向函数
+void Go_Straight()
+{
+    digitalWrite(R_IN1, 0x0);
+    digitalWrite(R_IN2, 0x1);
+    digitalWrite(L_IN3, 0x0);
+    digitalWrite(L_IN4, 0x1);
+}
+
+void Slow_TurnLeft()
+{
+    digitalWrite(R_IN1,0x0);
+    analogWrite(R_IN2,255);
+    digitalWrite(L_IN3,0x0);
+    analogWrite(L_IN4,220);
+}
+
+void Slow_TurnRight()
+{
+    digitalWrite(R_IN1,0x0);
+    analogWrite(R_IN2,220);
+    digitalWrite(L_IN3,0x0);
+    analogWrite(L_IN4,255);
+}
+
+void Fast_TurnLeft()
+{
+    digitalWrite(R_IN1,0x0);
+    analogWrite(R_IN2,255);
+    digitalWrite(L_IN3,0x0);
+    analogWrite(L_IN4,200);
+}
+
+void Fast_TurnRight()
+{
+    digitalWrite(R_IN1,0x0);
+    analogWrite(R_IN2,200);
+    digitalWrite(L_IN3,0x0);
+    analogWrite(L_IN4,255);
+}
+
+void Stop()
+{
+    digitalWrite(R_IN1, 0x0);
+    digitalWrite(R_IN2, 0x0);
+    digitalWrite(L_IN3, 0x0);
+    digitalWrite(L_IN4, 0x0);
+}
+
+
+
+//现在有两种算法，一种是单纯if判断，另一种是给每个传感器赋个特殊的值，然后用switch语句
+//先把if的写了
+
 void loop()
 {
-    int SensorVal_x1 = digitalRead(Sensor_x1);
-    int SensorVal_x2 = digitalRead(Sensor_x2);
-    int SensorVal_x3 = digitalRead(Sensor_x3);
-    int SensorVal_x4 = digitalRead(Sensor_x4);
+    int SensorVal_L_X1 = digitalRead(Sensor_L_X1);
+    int SensorVal_LL_X2 = digitalRead(Sensor_LL_X2);
+    int SensorVal_R_X3 = digitalRead(Sensor_R_X3);
+    int SensorVal_RR_X4 = digitalRead(Sensor_RR_X4);
 
-    //控制正反转
-    if (SensorVal_x1 && SensorVal_x3)
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 0 && SensorVal_R_X3 == 1 && SensorVal_RR_X4 == 1)
     {
-        digitalWrite(In1, 0x0);
-        digitalWrite(In2, 0x1);
-        digitalWrite(In3, 0x0);
-        digitalWrite(In4, 0x1);
-    }
-    else
-    {
-        digitalWrite(In1, 0x0);
-        digitalWrite(In2, 0x0);
-        digitalWrite(In3, 0x0);
-        digitalWrite(In4, 0x0);
-
+        Slow_TurnLeft();
     }
 
-    delay(10);
-    // else
-    // {
-    //     digitalWrite(In1,HIGH);
-    //     digitalWrite(In2,LOW);
-    // }
-# 71 "d:\\arduino\\arduino_src\\testdelay1\\testdelay.ino"
-    // digitalWrite(4,LOW);
-    // digitalWrite(5,HIGH);
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 1 && SensorVal_R_X3 == 0 && SensorVal_RR_X4 == 1)
+    {
+        Slow_TurnRight();
+    }
+
+    if(SensorVal_LL_X2 == 0 && SensorVal_L_X1 == 1 && SensorVal_R_X3 == 1 && SensorVal_RR_X4 == 1)
+    {
+        Fast_TurnLeft();
+    }
+
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 1 && SensorVal_R_X3 == 1 && SensorVal_RR_X4 == 0)
+    {
+        Fast_TurnRight();
+    }
+
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 0 && SensorVal_R_X3 == 0 && SensorVal_RR_X4 == 1)
+    {
+        Go_Straight();
+    }
+
+    if(SensorVal_LL_X2 == 1 && SensorVal_L_X1 == 1 && SensorVal_R_X3 == 1 && SensorVal_RR_X4 == 1)
+    {
+        Stop();
+    }
+
+    delay(1);
+
 }
